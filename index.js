6c0b199ada0b1e44df08bbb448a76b12727c1307
prahaladbelavadi-anvil <github@belavadi.com>
2022-10-21 01:41:01 -0700

    clean up comments and check ssh

--- index.js
+++ index.js
@@ -10,14 +10,3 @@ const client = new NetlifyAPI(NETLIFY_AUTH_TOKEN)
 const sites = await client.listSites()
 
 console.log(sites.map(obj=> obj.url))
-
-// Create a site. Notice `body` here for sending OpenAPI body
-// const site = await client.createSite({
-//   body: {
-//     name: `my-awesome-site`,
-//     // ... https://open-api.netlify.com/#/default/createSite
-//   },
-// })
-
-// // Delete site. Notice `site_id` is a path parameter https://open-api.netlify.com/#/default/deleteSite
-// await client.deleteSite({ site_id: siteId })
